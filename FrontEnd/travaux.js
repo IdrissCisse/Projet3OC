f6309b5934651ba2993c5fa193263dce07ebc138
Idriss Cisse <aliouc140@gmail.com>
2024-05-06 22:30:20 +0200

    Fixation du style appliqué aux boutons lors du clic

--- FrontEnd/travaux.js
+++ FrontEnd/travaux.js
@@ -1,64 +1,68 @@
-// Récuperaton des données par l'API
+// Récupération des données par l'API
 const reponse = await fetch("http://localhost:5678/api/works");
 const travaux = await reponse.json();
 
-// Fonction recuperant les travaux filtrés en argument (fonction suivante) pour les afficher
+// Fonction pour afficher les travaux filtrés
 async function afficherTravaux(travauxFiltres) {
+    const galerie = document.querySelector(".gallery");
+    galerie.innerHTML = "";
+    for (let travail of travauxFiltres) {
+        const figure = document.createElement("figure");
+        figure.innerHTML = `
+            <img src="${travail.imageUrl}" alt="${travail.title}">
+            <figcaption>${travail.title}</figcaption>
+        `;
+        galerie.appendChild(figure);
+    }
+}
 
-   // Recupère la div de la galerie 
-   const galerie = document.querySelector(".gallery");
-   galerie.innerHTML = "";
-
-   // Creation d'une structure HTML pour chaque travail dans la liste filtrée 
-   for (let travail of travauxFiltres) {
-    const figure = document.createElement("figure");
-    figure.innerHTML = `
-        <img src="${travail.imageUrl}" alt="${travail.title}">
-        <figcaption>${travail.title}</figcaption>
-    `;
-    galerie.appendChild(figure); // Place la balise "figure" comme enfant de l'element ".gallery" 
-   };
-   
-};
-afficherTravaux(travaux); // Afficher l'ensenmble des travaux par defaut lors du chargement de la page
+afficherTravaux(travaux);
 
 // Fonction pour les filtres de la galerie 
 async function filtrerGalerie() {
-
-    // Recuperation des categories depuis l'api
+    // Récupération des catégories depuis l'API
     const resp = await fetch("http://localhost:5678/api/categories");
-    const categories = await  resp.json();
+    const categories = await resp.json();
 
-    console.log(categories);
-
-    // Conversion du tableau  en Set 
+    // Conversion du tableau en Set pour éliminer les doublons
     const categoriesSet = new Set(categories.map(categorie => categorie.name));
 
-    // Creation  du menu de filtres
+    // Création du menu de filtres
     const filtres = document.querySelector(".filtres");
     const btnTous = document.createElement("button");
     btnTous.textContent = "Tous";
     btnTous.classList.add("bouton-filtre");
     filtres.appendChild(btnTous);
-   
-    // Afficher l'ensemble des travaux au clic du bouton " Tous "
-    btnTous.addEventListener("click", async () => {
-        await afficherTravaux(travaux)
-    })
 
-    // Creation de chaque bouton à partir des categories récupérées depuis l'API
+    // Afficher tous les travaux au clic du bouton "Tous"
+    btnTous.addEventListener("click", async () => {
+        await afficherTravaux(travaux);
+    });
+
+    // Création de chaque bouton à partir des catégories récupérées depuis l'API
     for (let categorieName of categoriesSet) {
         const btnFiltre = document.createElement("button");
         btnFiltre.textContent = categorieName;
         btnFiltre.classList.add("bouton-filtre");
         filtres.appendChild(btnFiltre);
 
-        // Filtre des travaux en fonction de la categorie correspondante au nom du bouton de filtre cliqué
+        // Filtrage des travaux en fonction de la catégorie correspondante au nom du bouton de filtre cliqué
         btnFiltre.addEventListener("click", async () => {
             const travauxFiltres = travaux.filter(travail => travail.category.name === categorieName);
             await afficherTravaux(travauxFiltres);
-            console.log(travauxFiltres)
+            console.log(travauxFiltres);
         });
-    };
-};        
+    }
+
+    const boutonsFiltre = document.querySelectorAll(".bouton-filtre");
+    for (const bouton of boutonsFiltre) {
+        bouton.addEventListener("click", () => {
+            for (const b of boutonsFiltre) {
+                b.classList.remove("focus");
+            }
+            bouton.classList.add("focus");
+        });
+    }
+}
+
 filtrerGalerie();
